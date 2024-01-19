a6df00104afabd6f0da95c96d28019432ba2cf4e
Stefan Roets <stefanroetscode@gmail.com>
2024-01-19 15:20:01 +0200

    Creating the Starts

---
+++ src/MainContainer.jsx
@@ -0,0 +1,13 @@
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls, } from "@react-three/drei";
+import AnimatedStars   from "./AnimatedStars.jsx";
+
+const MainContainer = () => {
+    return <Canvas>
+        <color attach='background' args={['black']} />
+        <OrbitControls/>
+        <AnimatedStars/>
+    </Canvas>
+}
+
+export default MainContainer
